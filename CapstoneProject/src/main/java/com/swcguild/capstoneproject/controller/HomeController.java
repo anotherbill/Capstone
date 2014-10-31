d4be7304457fa14b576eef59168f1061c6b82c2d
Colin Purvis <purvis.dc@gmail.com>
2014-10-31 13:56:12 -0400

    suppy set of AssetTypes

--- CapstoneProject/src/main/java/com/swcguild/capstoneproject/controller/HomeController.java
+++ CapstoneProject/src/main/java/com/swcguild/capstoneproject/controller/HomeController.java
@@ -32,16 +32,6 @@ public class HomeController {
         this.eventDao = eventDao;
         this.userDao = userDao;
     }
-//    
-//    @Inject
-//    public HomeController(AssetInterface assetDao, EventInterface eventDao, UserInterface userDao, NoteInterface noteDao, ReportInterface reportDao) {
-//        this.assetDao = assetDao;
-//        this.eventDao = eventDao;
-//        this.userDao = userDao;
-//        this.noteDao = noteDao;
-//        this.reportDao = reportDao;
-//    }
-//    
 
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String home() {
@@ -50,34 +40,21 @@ public class HomeController {
 
     @RequestMapping(value = {"/assets"}, method = RequestMethod.GET)
     public String displayBrowseAssets(Model model, HttpServletRequest request) {
+        Set<AssetType> types = getSelectedAssetTypes(request.getParameter("searchByCategory"));
+        model.addAttribute("types", types);
+
+        return "browseAssets";
+        /*
         Set<Category> categories = assetDao.getAllCategories();
-        Set<AssetType> types = new HashSet<>();
         Set<Asset> assets = new HashSet<>();
-        
         Category selectedCat = null;
         AssetType selectedType = null;
+
+        String typeName = request.getParameter("searchByType");
         
-        String catName = request.getParameter("searchByCategory");
-        String typeName = request.getParameter("searchByCategory");
-
-        //find desired category, if selected, and get asset types for category
-        for (Category cat : categories) {
-            if (cat.getCategoryName().equalsIgnoreCase(catName)) {
-                selectedCat = cat;
-                types.addAll(assetDao.getAssetTypeByCategory(cat));
-            }
-        }
-
-        //get all asset types if no category selected
-        if (selectedCat == null || catName == null || catName.equalsIgnoreCase("All")) {
-            for (Category cat : categories) {
-                types.addAll(assetDao.getAssetTypeByCategory(cat));
-            }
-        }
-
         if (typeName != null) {
             //find desired asset type, if selected, and get assets for asset type
-            for (AssetType type: types) {
+            for (AssetType type : types) {
                 if (type.getName().equalsIgnoreCase(typeName)) {
                     selectedType = type;
                     assets.addAll(assetDao.getAllAssetsByAssetType(selectedType));
@@ -85,106 +62,77 @@ public class HomeController {
             }
 
             //get all assets if no asset type selected
-            if (catName.equalsIgnoreCase("All") || selectedCat == null) {
+            if (typeName.equalsIgnoreCase("All") || selectedCat == null) {
                 assets.addAll(assetDao.getAllAssets());
             }
-            
+
             //if searching by asset type, supply requested set of assets
             model.addAttribute("assets", assets);
-        }
-        else{
+        } else {
             //if searching by category, supply requested set of asset types
             model.addAttribute("types", types);
         }
-
         
-        return "browseAssets";
+        */
     }
 
     @RequestMapping(value = {"/addAsset"}, method = RequestMethod.GET)
     public String displayAddAsset(Model model) {
         model.addAttribute("categories", assetDao.getAllCategories());
         model.addAttribute("assetTypes", assetDao.getAllAssetTypes());
-        
+
         model.addAttribute("newAsset", new Asset());
-        
+
         return "addAsset";
     }
 
     @RequestMapping(value = {"/manage_assets"}, method = RequestMethod.GET)
-    public String displayManageAssets(Model model) {
+    public String displayManageAssets(Model model, HttpServletRequest request) {
+        Set<AssetType> types = getSelectedAssetTypes(request.getParameter("searchByCategory"));
+        model.addAttribute("assetTypeList", types);
         return "manageAssets";
     }
 
     @RequestMapping(value = {"/submitNewAsset"}, method = RequestMethod.POST)
     public String submitNewAsset(@ModelAttribute("newAsset") Asset newAsset, Model model, HttpServletRequest request) {
         int typeId;
-        
-//        Asset newAsset = new Asset();
-//        Set<Category> categories = assetDao.getAllCategories();
-//        Set<AssetType> types;
-//        Category selectedCat = null;
-//        AssetType typeSelected = null;
-//
-//        String name = request.getParameter("name");
-//        String catDescription = request.getParameter("category");
-//        String type = request.getParameter("assetType");
-//        String status = request.getParameter("status");
-//        String serial = request.getParameter("serialNum");
-//        String imagePath = request.getParameter(""); //not sure how to handle image file. Will investigate.
-//        String damage = request.getParameter("damage");
-//        String note = request.getParameter("assetNote");
-//
-//        int serialNum;
-//
-//        try {
-//            serialNum = Integer.parseInt(serial);
-//        } catch (NumberFormatException e) {
-//            return "redirect:addAsset";
-//        }
-//
-//        for (Category c : categories) {
-//            if (c.getCategoryName().equalsIgnoreCase(catDescription)) {
-//                selectedCat = c;
-//            }
-//        }
-//        if (selectedCat == null) {
-//            selectedCat = new Category();
-//            selectedCat.setCategoryName(catDescription);
-//            assetDao.addCategory(selectedCat);
-//        }
-//
-//        types = assetDao.getAssetTypeByCategory(selectedCat);
-//        for (AssetType t : types) {
-//            if (t.getName().equalsIgnoreCase(type) || t.getName().equalsIgnoreCase(name)) {
-//                typeSelected = t;
-//            }
-//        }
-//        if (typeSelected == null) {
-//            typeSelected = new AssetType();
-//            typeSelected.setCategory(selectedCat);
-//            typeSelected.setName(name);
-//            typeSelected.setImagePath(imagePath);
-//            assetDao.addAssetType(typeSelected);
-//        }
-//
-//        newAsset.setAssetType(typeSelected);
-//        newAsset.setDamageStatus(damage);
-//        newAsset.setInStock(status.equalsIgnoreCase("available"));
-//        newAsset.setSerialNumber(serialNum);
-        
-        try{
+
+        try {
             typeId = Integer.parseInt(request.getParameter("typeId"));
-        }
-        catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return "addAsset";
         }
-        
+
         newAsset.setAssetType(assetDao.getAssetTypeById(typeId));
         assetDao.addAsset(newAsset);
-        
+
         //assetDao.addNoteToAsset(newAsset.getAssetId(), note, damage);
-        
         return "redirect:manage_assets";
+    }
+
+    private Set<AssetType> getSelectedAssetTypes(String categoryName) {
+        Set<Category> categories = assetDao.getAllCategories();
+        Set<AssetType> types = new HashSet<>();
+        Category selectedCat = null;
+        //String catName = request.getParameter("searchByCategory");
+
+        //find desired category, if selected, and get asset types for category
+        if (categoryName != null) {
+            for (Category cat : categories) {
+                if (cat.getCategoryName().equalsIgnoreCase(categoryName)) {
+                    selectedCat = cat;
+                    types.addAll(assetDao.getAssetTypeByCategory(cat));
+                }
+            }
+        }
+
+        //get all asset types if no category selected
+        if (selectedCat == null /* || catName == null || catName.equalsIgnoreCase("All")*/) {
+            for (Category cat : categories) {
+                types.addAll(assetDao.getAssetTypeByCategory(cat));
+            }
+        }
+
+        return types;
     }
 }
